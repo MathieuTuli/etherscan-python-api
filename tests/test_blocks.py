69ce8de45ba61d97ea1eb90af129c5c4c3d9ae64
Mathieu Tuli <tuli.mathieu@gmail.com>
2022-02-20 15:12:58 -0500

    Add other block tests

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -3,6 +3,8 @@ import time
 
 from py_etherscan.api.blocks import (
     BlockReward,
+    BlockCountdown,
+    BlockNoByTime
 )
 from py_etherscan.client import Client
 
@@ -17,3 +19,16 @@ def test_block_reward() -> None:
     time.sleep(1)
     endpoint = client.get(BlockReward(blockno=2165403,
                                       key=data['key']))
+
+
+def test_block_countdown() -> None:
+    time.sleep(1)
+    endpoint = client.get(BlockCountdown(blockno=16701588,
+                                         key=data['key']))
+
+
+def test_block_by_time() -> None:
+    time.sleep(1)
+    endpoint = client.get(BlockNoByTime(timestamp=1578638524,
+                                        closest='before',
+                                        key=data['key']))
